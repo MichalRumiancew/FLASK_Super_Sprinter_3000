79dea8dd643e93ac20803a2662f3fa985c10fd53
MichalRumiancew <michalr11@gmail.com>
2020-11-24 11:24:33 +0100

    not working

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask
-from  in_memory_data import user_stories
-
+from flask import Flask, render_template
+from in_memory_data import user_stories
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return user_stories[0]["title"]
+def index():
+    table_headers = ["Id", "Story Title", "User Story", "Acceptance criteria", "Business Value", "Estimation", "Status"]
+    user_story_keys = ["id", "title", "story", "criteria", "business", "estimation", "status"]
+    return render_template("index.html", table_headers=table_headers, user_stories=user_stories,
+                           user_story_keys=user_story_keys)
 
 
 if __name__ == '__main__':
